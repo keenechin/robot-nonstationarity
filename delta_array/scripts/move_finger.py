a441655938ffa40aaa5c6e7cabbcfece12dbf808
Keene Chin <keene.chin@gmail.com>
2021-09-16 15:44:58 -0400

    initial delta array, with existing bugs

---
+++ delta_array/scripts/move_finger.py
@@ -0,0 +1,44 @@
+from DeltaArray import DeltaArray
+import numpy as np
+from time import sleep
+
+da = DeltaArray(f"/dev/ttyACM0")
+
+# PRESET POSITIONS
+p = np.ones((8, 12)) * 0.0012
+p[0, 3:6] = np.array([0.01, 0.01, 0.01])
+p[1, 3:6] = np.array([0.02, 0.01, 0.01])
+p[2, 3:6] = np.array([0.01, 0.02, 0.01])
+p[3, 3:6] = np.array([0.01, 0.01, 0.02])
+
+p[4, 3:6] = np.array([0.02, 0.02, 0.02])
+p[5, 3:6] = np.array([0.03, 0.02, 0.02])
+p[6, 3:6] = np.array([0.02, 0.03, 0.02])
+p[7, 3:6] = np.array([0.02, 0.02, 0.03])
+
+
+def print_posn():
+    da.wait_until_done_moving()
+    posn = da.get_joint_positions()
+    print(posn[3:6])  # Motors 4-6
+
+
+def retract():
+    da.reset()
+    print_posn()
+
+
+retract()
+# print_posn()
+for i in range(0, 8): # LOOP THROUGH ALL PRESET POSITIONS
+    duration = [1.0]
+    print(f"i:{i}, {p[i,:]}")
+    da.move_joint_position(p[i, :].reshape(1,12), duration)
+    da.wait_until_done_moving()
+    sleep(1)
+    # print_posn()
+
+# RESET TO FULLY RETRACTED ACTUATORS
+retract()
+
+da.close()
