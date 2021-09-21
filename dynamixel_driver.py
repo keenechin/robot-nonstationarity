d1aafe76aeda8efa29cc665cc0823c9c1f915a7e
Keene Chin <keene.chin@gmail.com>
2021-09-21 11:37:39 -0400

    parameterized driver by number of servos

--- dynamixel_driver.py
+++ dynamixel_driver.py
@@ -2,12 +2,13 @@ import os
 import sys, tty, termios
 import dynamixel_sdk as dmx
 
-# Designed to control 2 XC-430-150-T servos from linux using U2D2
+# Designed to control dynamixel XC-430-150-T servos from linux using U2D2
 
 class DynamixelDriver():
-    def __init__(self):
+    def __init__(self, num_servos=1):
         self.fd = sys.stdin.fileno()
         self.old_settings = termios.tcgetattr(self.fd)
+
 
         self.ADDR_TORQUE_ENABLE          = 64
         self.ADDR_GOAL_POSITION          = 116
@@ -21,13 +22,13 @@ class DynamixelDriver():
         PROTOCOL_VERSION            = 2.0
 
         DEVICENAME                  = '/dev/ttyUSB0'
-        self.DXL_IDS                     = [1, 2]
+        self.num_servos = num_servos
+        self.DXL_IDS                     = list(range(1, num_servos+1))
         self.TORQUE_ENABLE               = 1
         self.TORQUE_DISABLE              = 0
         self.DXL_MOVING_STATUS_THRESHOLD = 20
         self.portHandler = dmx.PortHandler(DEVICENAME)
         self.packetHandler = dmx.PacketHandler(PROTOCOL_VERSION)
-
         self.connect()
 
         # Enable Dynamixel Torque
@@ -68,38 +69,38 @@ class DynamixelDriver():
 
     def servo_move(self, pos, verbose=True):
         # Write goal position
-        for dxl_id in self.DXL_IDS:
-            dxl_comm_result, dxl_error = self.packetHandler.write4ByteTxRx(self.portHandler, dxl_id, self.ADDR_GOAL_POSITION, pos)
+        for i, dxl_id in enumerate(self.DXL_IDS):
+            dxl_comm_result, dxl_error = self.packetHandler.write4ByteTxRx(self.portHandler, dxl_id, self.ADDR_GOAL_POSITION, pos[i])
             if dxl_comm_result != dmx.COMM_SUCCESS:
                 print("%s" % self.packetHandler.getTxRxResult(dxl_comm_result))
             elif dxl_error != 0:
                 print("%s" % self.packetHandler.getRxPacketError(dxl_error))
-        if verbose:
-            # Read present position
-            while 1:
-                for dxl_id in self.DXL_IDS:
-                    dxl_present_position, dxl_comm_result, dxl_error = self.packetHandler.read4ByteTxRx(self.portHandler, dxl_id, self.ADDR_PRESENT_POSITION)
-                    if dxl_comm_result != dmx.COMM_SUCCESS:
-                        print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
-                    elif dxl_error != 0:
-                        print("%s" % packetHandler.getRxPacketError(dxl_error))
+        while True:
+            for i, dxl_id in enumerate(self.DXL_IDS):
+                dxl_present_position, dxl_comm_result, dxl_error = self.packetHandler.read4ByteTxRx(self.portHandler, dxl_id, self.ADDR_PRESENT_POSITION)
+                if dxl_comm_result != dmx.COMM_SUCCESS and verbose:
+                    print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+                elif dxl_error != 0 and verbose:
+                    print("%s" % packetHandler.getRxPacketError(dxl_error))
 
-                    print("[ID:%03d] GoalPos:%03d  PresPos:%03d" % (dxl_id, dxl_goal_position[index], dxl_present_position))
+                if verbose:
+                    print("[ID:%03d] GoalPos:%03d  PresPos:%03d" % (dxl_id, pos[i], dxl_present_position))
 
-                if abs(dxl_goal_position[index] - dxl_present_position) < self.DXL_MOVING_STATUS_THRESHOLD:
-                    break
+            if abs(pos[i] - dxl_present_position) < self.DXL_MOVING_STATUS_THRESHOLD:
+                break
 
 
 
     def test(self):
         index = 0
-        dxl_goal_position = [self.DXL_MINIMUM_POSITION_VALUE, self.DXL_MAXIMUM_POSITION_VALUE]         # Goal position
+        dxl_goal_position = [[self.DXL_MINIMUM_POSITION_VALUE for i in range(self.num_servos)],
+                             [self.DXL_MAXIMUM_POSITION_VALUE for i in range(self.num_servos)]]
+        # Goal position
         # Test Motor Movements
         while 1:
             print("Press any key to continue! (or press ESC to quit!)")
             if self.getch() == chr(0x1b):
                 break
-
             self.servo_move(dxl_goal_position[index], verbose=False)
             # Change goal position
             if index == 0:
@@ -122,7 +123,7 @@ class DynamixelDriver():
 
 
 if __name__ == "__main__":
-    robot = DynamixelDriver()
+    robot = DynamixelDriver(2)
     robot.test()
 
 
