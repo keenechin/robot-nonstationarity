887247904a7d577b0ce557bd033ab20488489e32
Keene Chin <keene.chin@gmail.com>
2021-10-01 13:53:48 -0400

    modified bounds s.t. sequential random primitives don't lead to crashes

--- five_bar.py
+++ five_bar.py
@@ -10,7 +10,8 @@ class FiveBar():
         self.servos = dxl(motor_id=ids, motor_type=motor_type,
                           devicename=device, baudrate=baudrate, protocol=protocol)
         self.servos.open_port()
-        self.theta_range = 2*np.pi*(0.25)
+        self.theta_range = 2*np.pi*(0.5)
+        self.theta_diff_max = np.pi/6
         self.theta1_mid = 2*np.pi*(0.5+0.06)
         self.theta2_mid = 2*np.pi*(0.5-0.06)
         self.theta1_min = self.theta1_mid - self.theta_range/2
@@ -22,6 +23,7 @@ class FiveBar():
         self.linear_min = 0.01
         self.linear_zeros = np.ones((1, 12))*self.linear_min
         self.linear_max = 0.0375
+        self.linear_range = 0.0375-0.01
 
         self.reset()
 
@@ -39,15 +41,23 @@ class FiveBar():
         time.sleep(0.01)
         # self.linear.wait_until_done_moving()
 
-    def move_abs(self, pos1, pos2, thresh=0.01):
+    def move_abs(self, pos1, pos2, err_thresh=0.1):
         print(f"Raw val: {pos1:.3f},{pos2:.3f}")
         pos1 = np.clip(pos1, self.theta1_min, self.theta1_max)
         pos2 = np.clip(pos2, self.theta2_min, self.theta2_max)
-        print(f"Bounded: {pos1:.3f},{pos2:.3f}")
+        mean = (pos1+pos2)/2
+        pos1 = np.clip(pos1, mean-self.theta_diff_max /
+                       2, mean+self.theta_diff_max/2)
+        pos2 = np.clip(pos2, mean-self.theta_diff_max /
+                       2, mean+self.theta_diff_max/2)
+        print(f"Bounded: {pos1:.3f},{pos2:.3f}\n")
+
         self.servos.set_des_pos(self.servos.motor_id, [pos1, pos2])
+
+        # Wait till done moving
         err1 = np.inf
         err2 = np.inf
-        while err1 > thresh or err2 > thresh:
+        while err1 > err_thresh or err2 > err_thresh:
             curr = self.get_pos()
             err1 = np.abs(curr[0]-pos1)
             err2 = np.abs(curr[1]-pos2)
@@ -64,44 +74,35 @@ class FiveBar():
         curr = self.servos.get_pos(self.servos.motor_id)
         return curr
 
-    def primitive(self, idx, mag=0.2*np.pi):
-        if idx == 1:
-            self.move_delta(-mag, -mag)
-        if idx == 2:
-            self.move_delta(-mag, 0.0)
-        if idx == 3:
-            self.move_delta(-mag, mag)
-        if idx == 4:
-            self.move_delta(0.0, -mag)
-        if idx == 5:
-            self.move_delta(0.0, 0.0)
-        if idx == 6:
-            self.move_delta(0.0, mag)
-        if idx == 7:
-            self.move_delta(mag, -mag)
-        if idx == 8:
-            self.move_delta(mag, 0.0)
-        if idx == 9:
-            self.move_delta(mag, mag)
+    def primitive(self, id, mag=0.2*np.pi):
+
+        primitives = [[-mag, -mag],
+                      [-mag, 0.0],
+                      [-mag, mag],
+                      [0.0, -mag],
+                      [0.0, 0.0],
+                      [0.0, mag],
+                      [mag, -mag],
+                      [mag, 0.0],
+                      [mag, mag]]
+
+        self.move_delta(*primitives[id])
 
     def trajectory(self, primitive_list):
         for idx in primitive_list:
             self.primitive(idx)
 
     def test_motion(self):
-        self.trajectory([5, 5, 5, 1, 1, 1, 1, 9, 9, 9, 9])
-        self.drift(self.linear_max)
-        # self.trajectory([1, 9, 2, 8, 3, 7, 4, 6, 5])
-        self.trajectory([2, 8, 8, 2, 2, 8])
-        self.trajectory([4, 6, 6, 4, 4, 6])
-        self.drift(self.linear_min)
-        # self.trajectory([1, 3, 9, 7]*2)
-        self.trajectory([2, 8, 8, 2, 2, 8])
-        self.trajectory([4, 6, 6, 4, 4, 6])
+        for j in range(10):
+            for i in range(100):
+                n = np.random.randint(0, 9)
+                self.primitive(n)
+            self.drift(np.random.random_sample()*self.linear_range + self.linear_min)
+            self.move_abs(self.theta1_mid, self.theta2_mid)
         self.reset()
 
 
-DESC = ''' 
+DESC = '''
 USAGE:
 python five_bar.py --motor_id "[1,2]" --motor_type "X" --baudrate 1000000 --device /dev/ttyUSB0 --protocol 2
 '''
