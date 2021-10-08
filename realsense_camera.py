ae85e0f7a12a52f526d8db76a03bf079b8db212e
Keene Chin <keene.chin@gmail.com>
2021-10-08 11:02:48 -0400

    figured out zero-motion tracking failure, eliminated medianflow as solution, non-multiprocessing single file

--- realsense_camera.py
+++ realsense_camera.py
@@ -1,22 +1,18 @@
 import pyrealsense2 as rs
 import cv2
 from multiprocessing import Process, Queue
+import time
 import numpy as np
 from queue import Empty
 
 
-
 class RealsenseCamera():
     def __init__(self):
-        self.camera = rs.pipeline()
-        self.config = rs.config()
-        self.config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
-
-    def get_rgb(self):
-        frames = self.camera.wait_for_frames()
-        color_frame = frames.get_color_frame()
-        frame = np.asanyarray(color_frame.get_data())
-        return frame
+        camera = rs.pipeline()
+        config = rs.config()
+        config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+        camera.start(config)
+        self.camera = camera
 
     def create_tracker(self, tracker_name):
         OPENCV_OBJECT_TRACKERS = {
@@ -28,43 +24,51 @@ class RealsenseCamera():
             "medianflow": cv2.TrackerMedianFlow_create,
             "mosse": cv2.TrackerMOSSE_create
         }
-
         return OPENCV_OBJECT_TRACKERS[tracker_name]()
 
-    def initialize_tracking(self, trackerType="csrt"):
+    def initialize_tracking(self, trackerType="mosse"):  # medianflow
         bboxes = []
-        self.camera.start(self.config)
         multi_tracker = cv2.MultiTracker_create()
-        for i in range(100): # give camera time to autoadjust
+        for i in range(100):  # give camera time to autoadjust
             frame = self.get_rgb()
 
         while True:
-            bbox = cv2.selectROI('MultiTracker', frame, fromCenter=False, showCrosshair=True)
+            init_window = 'MultiTracker selections (MAKE BIG BOXES)'
+            bbox = cv2.selectROI(init_window, frame, fromCenter=False, showCrosshair=True)
             bboxes.append(bbox)
             print("Press q to quit selecting boxes and start tracking")
             print("Press any other key to select next object")
             k = cv2.waitKey(0) & 0xFF
             if (k == ord('q')):  # q is pressed
                 break
-        cv2.destroyWindow('MultiTracker')
+        cv2.destroyWindow(init_window)
 
         bboxes = bboxes[:2]
+        print(bboxes)
         for bbox in bboxes:
             multi_tracker.add(self.create_tracker(trackerType), frame, bbox)
+        time.sleep(2)
         return bboxes, multi_tracker
 
     def get_feed(self):
         self.feed = Queue()
         bboxes, multi_tracker = self.initialize_tracking()
         # self.track_points(self.feed, bboxes, multi_tracker, True)
-        self.process = Process(target=self.track_points, args=(self.feed, bboxes, multi_tracker, True))
-        return self.feed
+        # self.process = Process(target=self.track_points, args=(self.feed, bboxes, multi_tracker, True))
+        return self.feed, bboxes, multi_tracker
+
+    def get_rgb(self):
+        frames = self.camera.wait_for_frames()
+        color_frame = frames.get_color_frame()
+        frame = np.asanyarray(color_frame.get_data())
+        return frame
 
     def track_points(self, queue, bboxes, multi_tracker, visualize=True):
         while True:
             frame = self.get_rgb()
             success, bboxes = multi_tracker.update(frame)
             if success:
+                print("Success.")
                 if not queue.empty():
                     try:
                         queue.get_nowait()
@@ -79,13 +83,21 @@ class RealsenseCamera():
                     cv2.circle(img=frame, center=state, radius=5, color=(0, 0, 200), thickness=-1)
 
                 queue.put(states)
+            else:
+                print("Fail.")
             if visualize:
                 cv2.imshow('Live Feed', frame)
                 k = cv2.waitKey(1)
                 if (k == ord('q')):  # q is pressed
                     break
 
-    def __del__(self):
-        cv2.destroyAllWindows()
-        self.process.terminate()
 
+    # def __del__(self):
+    #     cv2.destroyAllWindows()
+    #     self.process.terminate()
+
+
+if __name__ == "__main__":
+    camera = RealsenseCamera()
+    feed, bboxes, multi_tracker = camera.get_feed()
+    camera.track_points(feed, bboxes, multi_tracker)
