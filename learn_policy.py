ce0165ea3a3919d8d4375bd80b18eee252508ff9
Keene Chin <keene.chin@gmail.com>
2021-10-14 16:15:15 -0400

    manual hardware learning with learned policy

--- learn_policy.py
+++ learn_policy.py
@@ -4,6 +4,7 @@ from sklearn.neural_network import MLPRegressor
 import time
 import copy
 import pickle
+from functools import partial
 
 
 def test(env):
@@ -23,8 +24,11 @@ def rollout(env, policy, num_steps):
     X = []
     y = []
     state = env.reset()
+    nA = env.action_space.n
+
     for i in range(num_steps):
-        action = policy(state)
+        probs = policy(state)
+        action = np.random.choice(list(range(nA)), p=probs)
         next_state, reward, _, _ = env.step(action)
         X.append([*state, action])
         y.append([*next_state])
@@ -32,15 +36,15 @@ def rollout(env, policy, num_steps):
     return X, y
 
 
-def random_policy(_):
+def random_policy(state):
     return np.random.randint(env.action_space.n)
 
 
-def learn_model(env, N):
+def learn_model(env, policy, N):
     model = MLPRegressor(max_iter=5000)
     X = []
     y = []
-    X, y = rollout(env, random_policy, N)
+    X, y = rollout(env, policy, N)
     model.fit(X, y)
     print("Model Trained.")
     return model
@@ -55,8 +59,8 @@ def test_model(env, model, N):
     return errors
 
 
-def linear_softmax_policy(state, w):
-    z = state.dot(w)
+def linear_softmax_policy(state, params):
+    z = state.dot(params)
     exp = np.exp(z)
     return exp/np.sum(exp)
 
@@ -66,6 +70,30 @@ def softmax_grad(softmax):
     return np.diagflat(s) - np.dot(s, s.T)
 
 
+def learned_policy(state):
+    params = np.array([[1.30599275,  0.8440695,  0.58461806, -0.02011946,  0.03461973,
+                        0.54696131,  0.34817409,  0.09967214,  0.33051899],
+                       [1.06756715,  0.91685071,  0.51103562,  0.44438496,  0.51257505,
+                        0.59803864,  0.83532824,  0.47229371,  0.4040024],
+                       [1.04607871,  0.84804166,  0.01283282,  0.66361759,  0.79730298,
+                        0.67203189,  0.77656011,  0.44848724,  0.12165581],
+                       [1.09589905,  0.51317097,  0.16997307,  0.68079135,  0.96937808,
+                        0.22981028,  0.67614996,  0.484358,  0.71306881],
+                       [1.08442556,  0.39196105,  0.21902421,  0.3574361,  0.73008704,
+                        0.6766985,  0.46125652,  0.89495842,  0.44598672],
+                       [0.49545462,  0.76035037,  0.0471648,  0.35860607,  0.76418505,
+                        0.73295034,  0.30972206,  0.10925648,  0.46686322],
+                       [0.35234971,  0.72541968,  0.1852073,  0.32916013,  0.84524875,
+                        0.60527143,  0.89262316,  0.97511096,  0.83733929],
+                       [0.20997242,  0.60701566,  0.47026797,  0.39273162,  0.74685098,
+                        0.80762057,  0.6962998,  0.13834534,  0.70353977],
+                       [0.06485459,  0.19487276,  0.92483907,  0.40695581,  0.14517594,
+                        0.68020901,  0.15877936,  0.6467614,  0.25285785],
+                       [0.05769313,  0.95822918,  0.06329934,  0.51090626,  0.34163173,
+                        0.64786567,  0.84842912,  0.60230252,  0.58571582]])
+    return linear_softmax_policy(state, params)
+
+
 if __name__ == "__main__":
     gym.envs.register(
         id='FiveBarPendulum-v0',
@@ -73,13 +101,14 @@ if __name__ == "__main__":
         kwargs={}
     )
     env = gym.make('FiveBarPendulum-v0')
-    np.random.seed(3)
-    filename = 'hardware_model.sav'
+    np.random.seed(6)
+    N = 251
+    filename = f'hardware_model_{N}.sav'
     from os.path import exists
     if exists(filename):
         model = pickle.load(open(filename, 'rb'))
     else:
-        model = learn_model(env, 1000)
+        model = learn_model(env, learned_policy, N)
         pickle.dump(model, open(filename, 'wb'))
 
     # Learning Code Here
@@ -87,7 +116,7 @@ if __name__ == "__main__":
 
     start = time.time()
     NUM_EPISODES = 250
-    LEARNING_RATE = 0.0025
+    LEARNING_RATE = 0.000025
     GAMMA = 0.99
     nA = env.action_space.n
     episode_rewards = []
@@ -105,7 +134,8 @@ if __name__ == "__main__":
         for i in range(100):
             probs = policy(state, w)
             action = np.random.choice(list(range(nA)), p=probs[0])
-            next_state = model.predict(np.reshape([*state[0], action], (1, -1)))
+            next_state = model.predict(
+                np.reshape([*state[0], action], (1, -1)))
             reward = -env.cost(next_state[0])
             dsoftmax = softmax_grad(probs)[action, :]
             dlog = dsoftmax / probs[0, action]
@@ -124,7 +154,7 @@ if __name__ == "__main__":
 
         # Append for logging and print
         episode_rewards.append(score)
-        if score < best_score:
+        if score > best_score:
             best_w = w
             best_score = score
         print(f"EP: {str(e)} Score: {str(score)}")
